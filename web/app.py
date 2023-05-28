84661fa3d9a704ea87f1b7992c144c30b815de95
KiefBC <kiefhay642@outlook.com>
2023-05-27 21:30:53 -0700

    Stage 3 Complete.

--- web/app.py
+++ web/app.py
@@ -5,9 +5,7 @@ import sys
 app.add_url_rule('/', view_func=MainIndexView.as_view('index'))
 app.add_url_rule('/login', view_func=LoginView.as_view('login'))
 app.add_url_rule('/profile', view_func=ProfileView.as_view('profile'))
-app.add_url_rule('/add', view_func=AddCityView.as_view('add_city'))
-
-
+app.add_url_rule('/', view_func=AddCityView.as_view('add_city'))
 
 # don't change the following way to run flask:
 if __name__ == '__main__':
--- web/views.py
+++ web/views.py
@@ -1,5 +1,11 @@
+import requests
+import os
+from dotenv import load_dotenv
 from flask.views import MethodView
 from flask import render_template, request
+
+# load our API key from our .env file
+load_dotenv(override=True)
 
 
 class MainIndexView(MethodView):
@@ -22,5 +28,22 @@ class AddCityView(MethodView):
         return render_template('add_city.html')
 
     def post(self):
+        error = None
         city_name = request.form['city_name']
-        return 'City Added!!!!!!!!!!!!!'
+        r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv("API_KEY")}')
+        if r.status_code == 200:
+            data = r.json()
+            data_obj = {
+                'city': data['name'],
+                'temp': self.kelvin_celcius(data['main']['temp']),
+                'state': data['weather'][0]['main']
+            }
+            print(data)
+            return render_template('index.html', data=data_obj)
+        else:
+            error = 'City not found'
+            return render_template('add_city.html', error=error)
+
+    @staticmethod
+    def kelvin_celcius(kelvin: float) -> int:
+        return int(kelvin - 273.15)
